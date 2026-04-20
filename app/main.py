bee14a453ef2a47c2c169130c13862e419df02fe
Tymur <t.balenko12008@gmail.com>
2026-04-20 20:02:16 +0200

    resolveddd

--- app/main.py
+++ app/main.py
@@ -3,13 +3,8 @@ class Cargo:
         self.weight = weight
 
 
-class BaseRobot():
-    def __init__(
-            self,
-            name: str,
-            weight: int,
-            coords: list | None = None
-    ) -> None:
+class BaseRobot:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords is not None else [0, 0]
@@ -31,13 +26,11 @@ class BaseRobot():
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(
-            self,
-            name: str,
-            weight: int,
-            coords: list | None = None
-    ) -> None:
-        actual_coords = coords if coords is not None else [0, 0, 0]
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+        actual_coords = list(coords) if coords is not None else [0, 0, 0]
+        if len(actual_coords) == 2:
+            actual_coords.append(0)
+
         super().__init__(name, weight, actual_coords)
 
     def go_up(self, step: int = 1) -> None:
@@ -53,18 +46,14 @@ class DeliveryDrone(FlyingRobot):
             name: str,
             weight: int,
             max_load_weight: int,
-            current_load: Cargo | None = None,
-            coords: list | None = None
+            current_load: Cargo = None,
+            coords: list = None
     ) -> None:
-        if coords is None:
-            actual_coords = [0, 0, 0]
-        else:
-            actual_coords = list(coords)
-            if len(actual_coords) == 2:
-                actual_coords.append(0)
-        super().__init__(name, weight, actual_coords)
+        super().__init__(name, weight, coords)
+
         self.max_load_weight = max_load_weight
         self.current_load = None
+
         if current_load is not None:
             self.hook_load(current_load)
 
@@ -72,5 +61,5 @@ class DeliveryDrone(FlyingRobot):
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
-    def unhook_load(self,) -> None:
+    def unhook_load(self) -> None:
         self.current_load = None
