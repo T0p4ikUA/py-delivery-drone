23f5dad0c5f33c3dcc9af4d21ee31f724dda6593
Tymur <t.balenko12008@gmail.com>
2026-04-20 19:47:36 +0200

    resolved:

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ class Cargo:
 
 
 class BaseRobot():
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list | None = None
+    ) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords is not None else [0, 0]
@@ -26,7 +31,12 @@ class BaseRobot():
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list | None = None
+    ) -> None:
         actual_coords = coords if coords is not None else [0, 0, 0]
         super().__init__(name, weight, actual_coords)
 
@@ -43,10 +53,16 @@ class DeliveryDrone(FlyingRobot):
             name: str,
             weight: int,
             max_load_weight: int,
-            current_load: Cargo = None,
-            coords: list = None
+            current_load: Cargo | None = None,
+            coords: list | None = None
     ) -> None:
-        super().__init__(name, weight, coords)
+        if coords is None:
+            actual_coords = [0, 0, 0]
+        else:
+            actual_coords = list(coords)
+            if len(actual_coords) == 2:
+                actual_coords.append(0)
+        super().__init__(name, weight, actual_coords)
         self.max_load_weight = max_load_weight
         self.current_load = None
         if current_load is not None:
